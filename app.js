cb1e7f038a9e2d74def5c8f28059f15b930bdac8
Francesco Punta <puntafrancesco@gmail.com>
2023-03-22 16:18:34 +0100

    Step 2 Vue CDN & index.php

---
+++ app.js
@@ -0,0 +1,23 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        todos: []
+    },
+    mounted() {
+        axios.get('todo.php')
+            .then(response => {
+                this.todos = response.data;
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    },
+    template: `
+        <div>
+            <h1>Todo List</h1>
+            <ul>
+                <li v-for="todo in todos" :key="todo.id">{{ todo.title }}</li>
+            </ul>
+        </div>
+    `
+});
---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Todo List</title>
+    <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+</head>
+<body>
+    <div id="app">
+        <!-- Codice vue -->
+    </div>
+    <script src="app.js"></script>
+</body>
+</html>

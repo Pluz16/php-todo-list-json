ac292835173bf4e238d74b4455a0aa6e8393ac10
Francesco Punta <puntafrancesco@gmail.com>
2023-03-22 16:29:31 +0100

    Aggiunta funzionalità di invio di un nuovo task tramite Axios.

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var app = new Vue({
     el: '#app',
     data: {
-        todos: []
+        todos: [],
+        newTodo: ''
     },
     mounted() {
         axios.get('todo.php')
@@ -12,12 +13,31 @@ var app = new Vue({
                 console.log(error);
             });
     },
+    methods: {
+        addTodo() {
+            axios.post('todo.php', {
+                title: this.newTodo
+            })
+            .then(response => {
+                console.log(response.data);
+                this.todos.push(response.data);
+                this.newTodo = '';
+            })
+            .catch(error => {
+                console.log(error);
+            });
+        }
+    },
     template: `
         <div>
             <h1>Todo List</h1>
             <ul>
                 <li v-for="todo in todos" :key="todo.id">{{ todo.title }}</li>
             </ul>
+            <form @submit.prevent="addTodo">
+                <input type="text" v-model="newTodo">
+                <button type="submit">Add</button>
+            </form>
         </div>
     `
 });

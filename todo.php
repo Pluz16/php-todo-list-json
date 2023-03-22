4b8fc1705c6edc4392a5c4b4bfeb304915c9a66d
Francesco Punta <puntafrancesco@gmail.com>
2023-03-22 16:13:16 +0100

    Rename e creazione docs

---
+++ todo.php
@@ -0,0 +1,11 @@
+<?php
+
+$todos = array(
+    array("id" => 1, "title" => "Fare la spesa"),
+    array("id" => 2, "title" => "Lavare pavimento"),
+    array("id" => 3, "title" => "Studiare PHP"),
+);
+
+echo json_encode($todos);
+
+?>

05c255238db30f2140452a6a2024c21f2196ea4d
Francesco Punta <puntafrancesco@gmail.com>
2023-03-22 17:41:57 +0100

    Aggiunta stile. Problema con salvataggio dati.

--- index.php
+++ index.php
@@ -1,15 +1,20 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
     <meta charset="UTF-8">
-    <title>PHP Todo List</title>
-    <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <title>Todo App</title>
+    <style>
+        .completed {
+            text-decoration: line-through;
+        }
+    </style>
 </head>
 <body>
-    <div id="app">
-        <!-- Codice vue -->
-    </div>
+    <div id="app"></div>
+
+    <link rel="stylesheet" href="./style/style.css">
+    <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
     <script src="app.js"></script>
 </body>
 </html>
